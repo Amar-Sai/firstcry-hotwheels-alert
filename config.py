3127f9f7293ffb3581ec14d2cdb1e63f103ff077
Amar Sai <147689875+Amar-Sai@users.noreply.github.com>
2026-02-05 23:47:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 FIRSTCRY_LISTING_URL = "https://www.firstcry.com/Hot%20Wheels/0/0/113?q=as_hot%20wheels&asid=48299"
 FIRSTCRY_SEARCH_URL = "https://www.firstcry.com/search?q=hot+wheels"
+
 SEEN_FILE = "data/seen.json"
+
+# 🔥 High-signal Hot Wheels keywords
+HOTWHEELS_KEYWORDS = [
+    "f1",
+    "formula 1",
+    "formula-1",
+    "AMG"
+    "mercedes",
+    "mercedes-benz",
+    "ferrari",
+    "mclaren",
+    "porsche"
+]
+
